e8c554fafeb73156c851a0fe94a92f3affc96dcc
Arthur <lebelbarthur@gmail.com>
2018-01-29 10:43:36 -0500

    Ajout des informations de Arthur

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
 		competences[1] = "Programmation VB.Net";
 		CV fred = new CV("England", "Frédéric", "DEC en informatique de Gestion", 2, competences, "Aucunes");
 		
+		String[] comp = new String[1];
+		comp[0] = "Programmation Java";
+		CV arthur = new CV ("Lebel", "Arthur", "DEC en Informatique de Gestion", 1, comp, "Apprendre");
+		
 		System.out.println("Bienvenue chez Barette !");
 		fred.affiche();
 
