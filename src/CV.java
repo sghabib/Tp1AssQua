add9488612ff3fd52336700aa0c6feedb59b9420
Fred420 <fred908@hotmail.ca>
2018-01-29 09:19:32 -0500

    Ajout du constructeur de CV

--- src/CV.java
+++ src/CV.java
@@ -1,4 +1,22 @@
 
 public class CV {
-
+	
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int expTravail;
+	private String[] competences;
+	private String attentes;
+	
+	public CV ( String nom, String prenom, String formation, int expTravail, String[] competences, String attentes ) {
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.expTravail = expTravail;
+		this.competences = competences;
+		this.attentes = attentes;
+		
+	}
+	
 }
