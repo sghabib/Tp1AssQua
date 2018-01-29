edb54e93dd629d4074ce3fe26ef3ee7a29bdd70e
Fred420 <fred908@hotmail.ca>
2018-01-29 09:35:12 -0500

    Ajout de l'affichage des CV dans le Main, ajout des informations de Frederic

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] competences = new String[2];
+		competences[0] = "Programmation Java";
+		competences[1] = "Programmation VB.Net";
+		CV fred = new CV("England", "Frédéric", "DEC en informatique de Gestion", 2, competences, "Aucunes");
+		
+		System.out.println("Bienvenue chez Barette !");
+		fred.affiche();
 
 	}
 
